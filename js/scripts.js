b8492300ef1d375ecbdd68c172e6317059b09596
Stewart ilondanga <stilondanga@gmail.com>
2018-10-02 11:35:48 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -7,21 +7,14 @@ var navigate = (function() {
 	});
 })();
 
-$(function() {
-     var boardWidth = $('#kanban-board').width();
-     var $columns = $('.sortable-wrapper');
-     var columnCount = $columns.length;
-     var columnMargin = 10;
-     $columns.width(Math.floor((boardWidth - (columnMargin*(columnCount + 1))) / columnCount));
-     $( ".sortable" ).sortable({
-         revert: true,
-         placeholder: 'drag-place-holder',
-         forcePlaceholderSize: true,
-         connectWith: ".sortable",
-         helper:function(event, element){return $(element).clone().addClass('dragging');},
-         start: function (e, ui) {ui.item.show().addClass('ghost')},
-         stop: function (e, ui) { ui.item.show().removeClass('ghost')},
-         cursor:'move'
-     })
-     .disableSelection();;
-});
+function allowDrop(ev){
+  ev.preventDefault();
+}
+function drag(ev){
+  ev.dataTransfer.setData("Text",ev.target.id);
+}
+function drop(ev){
+  ev.preventDefault();
+  var data=ev.dataTransfer.getData("Text");
+  ev.target.appendChild(document.getElementById(data));
+}
