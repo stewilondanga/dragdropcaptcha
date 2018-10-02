98f2210ad1a2a3084c2bd547c69529f9ca28e22a
Stewart ilondanga <stilondanga@gmail.com>
2018-10-02 11:10:54 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,22 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+$(function() {
+     var boardWidth = $('#kanban-board').width();
+     var $columns = $('.sortable-wrapper');
+     var columnCount = $columns.length;
+     var columnMargin = 10;
+     $columns.width(Math.floor((boardWidth - (columnMargin*(columnCount + 1))) / columnCount));
+     $( ".sortable" ).sortable({
+         revert: true,
+         placeholder: 'drag-place-holder',
+         forcePlaceholderSize: true,
+         connectWith: ".sortable",
+         helper:function(event, element){return $(element).clone().addClass('dragging');},
+         start: function (e, ui) {ui.item.show().addClass('ghost')},
+         stop: function (e, ui) { ui.item.show().removeClass('ghost')},
+         cursor:'move'
+     })
+     .disableSelection();;
+});
