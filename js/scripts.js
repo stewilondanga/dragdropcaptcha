af022445b124589a36bfb62a2159680687b5d263
Stewart ilondanga <stilondanga@gmail.com>
2018-10-02 12:01:32 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,11 @@ var navigate = (function() {
 function allowDrop(ev) {
   ev.preventDefault();
 }
-/*function drag(ev){
-  ev.dataTransfer.setData("Text",ev.target.id);
+
+function drag(ev) {
+  ev.dataTransfer.setData("Text", ev.target.id);
 }
-function drop(ev){
+/*function drop(ev){
   ev.preventDefault();
   var data=ev.dataTransfer.getData("Text");
   ev.target.appendChild(document.getElementById(data));
